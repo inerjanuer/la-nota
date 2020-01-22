b473914a81d0b3102c2890b221248ade867caaf2
jelver <jelverfon@outlook.es>
2020-01-21 20:02:12 -0500

    ingenieria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './inicio/inicio.component';
 import { ProgramasComponent } from './programas/programas.component';
+import { IngenieriaComponent } from './ingenieria/ingenieria.component';
 
 
 const routes: Routes= [
   	{path: '', redirectTo: '/inicio', pathMatch: 'full'},
 	  {path: 'inicio', component: InicioComponent},
-    {path: 'programas', component: ProgramasComponent}
+    {path: 'programas', component: ProgramasComponent},
+    {path: 'ingenieria', component: IngenieriaComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    ProgramasComponent
+    ProgramasComponent,
+    IngenieriaComponent
   ],
   imports: [
     BrowserModule,
